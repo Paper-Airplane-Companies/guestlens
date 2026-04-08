5aceae2dabbda77d5eac1aa906f5ddf4a4ceca31
Brandonlabonte-owner <bgl73.brandonguy@gmail.com>
2026-04-08 11:09:55 -0400

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
+const supabaseUrl = 'https://csqjqsvmkzuiijkruefc.supabase.co'
+const supabaseKey = 'sb_publishable_qODf0-nrWhUCgUXk08lLRw_54WvjTnj'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
